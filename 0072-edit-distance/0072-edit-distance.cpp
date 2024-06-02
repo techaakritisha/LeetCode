da226fc5013237c249273442dbef8c6d01ba68e8
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-02 18:12:07 -0100

    Time: 11 ms (34.34%), Space: 12 MB (33.84%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int m , n ;
     int t[501][501];
     int solve(string &s1,string &s2 ,int i , int j) {
@@ -31,5 +32,30 @@ public:
         
         memset(t,-1,sizeof(t));
         return solve(word1,word2,0,0);
+        
+        
+        */
+    
+        int minDistance(string word1, string word2) {
+            
+            int m = word1.length();
+            int n = word2.length();
+            vector<vector<int>> t(m+1,vector<int>(n+1));
+                
+            for(int i=0; i<=m; i++) {
+                for(int j=0; j<=n; j++) {
+                    if(i==0 || j==0) {
+                        t[i][j] = i + j;
+                    }
+                    else if(word1[i-1] == word2[j-1]){
+                        t[i][j] = t[i-1][j-1];
+                    }
+                    else{
+                        t[i][j] = 1 + min({t[i-1][j],t[i][j-1],t[i-1][j-1]});
+                    } 
+                }
+            }
+    
+        return t[m][n];
     }
 };
