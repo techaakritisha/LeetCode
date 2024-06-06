4f3976936fbe769cc9b76d96c3c23cb45d06c213
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-06 11:04:50 -0100

    Time: 2 ms (52.77%), Space: 7.2 MB (74.05%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+//     int t[59];
+//     int solve(int n){
+        
+//        if(n == 1) 
+//          return 1;
+
+//          if(t[n] != -1)
+//            return t[n];
+
+//          int result = INT_MIN;
+
+//          for(int i=1; i<n; i++){
+//              int prod = i * max(n-i , solve(n-i));
+//              result = max(result , prod);
+//          }
+//          return t[n] = result;
+//     }
+    int t[59];
+    int solve(int n){
+        if(n == 1)
+            return 1;
+        
+        if(t[n] != -1)
+            return t[n];
+        
+        int result = INT_MIN;
+        
+        for(int i=1; i<n; i++){
+            int product = i * max(n-i , solve(n-i));
+            result = max(result,product);
+        }
+        return t[n] = result;
+    }
+    int integerBreak(int n) {
+
+       memset(t,-1,sizeof(t));
+       return solve(n);
+    }
+};
