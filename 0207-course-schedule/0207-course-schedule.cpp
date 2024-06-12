06286daa059a48297ca1e1806988e554698bb8f5
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-12 08:40:17 -0100

    Time: 17 ms (48.90%), Space: 18.9 MB (14.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
+    /*
    bool solve(unordered_map<int,vector<int>> adj,int n, vector<int> deg) {
-        queue<int> q;
+       queue<int> q;
        int count = 0;
        for(int i=0;i <n; i++) {
            if(deg[i] == 0) {
@@ -41,5 +42,45 @@ public:
             deg[a]++;
         }
         return solve(adj,numCourses,deg);
+        
+        */
+    
+       //  Using DFS
+      bool DFS( unordered_map<int,vector<int>> &adj,int u , vector<bool> &visited , vector<bool> &rec) {
+          visited[u] = true;
+          rec[u] = true;
+          
+          for(int &v : adj[u]){
+              if(!visited[v] && DFS(adj,v,visited,rec)){
+                  return true;
+              }
+              if(rec[v] == true){
+                  return true;
+              }
+          }
+          rec[u] = false;
+          return false;
+      }
+      bool canFinish(int numCourses, vector<vector<int>>& prerequisites){
+          
+            unordered_map<int,vector<int>> adj;
+           
+           for(auto &pre : prerequisites){
+               int a = pre[0];
+               int b = pre[1];
+               
+               adj[b].push_back(a);
+               
+           }
+          
+          vector<bool> visited(numCourses,false);
+          vector<bool> recur(numCourses,false);
+          
+          for(int i=0; i<numCourses; i++){
+              if(!visited[i] && DFS(adj,i,visited,recur)){
+                  return false;
+              }
+          }
+        return true;
     }
 };
