67b8d60024528abb8d65e96baa9f94ceb61305e5
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-13 16:36:15 -0100

    Time: 0 ms (100.00%), Space: 16.2 MB (62.77%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -9,10 +9,10 @@ public:
         if(t[i] != -1) 
              return t[i];
         
-        int op1 = solve(i+1,cost);
-        int op2 = solve(i+2,cost);
+        int op1 = solve(i+1,cost) + cost[i];
+        int op2 = solve(i+2,cost) + cost[i];
         
-        return t[i] = min(op1,op2)+cost[i];
+        return t[i] = min(op1,op2);
     }
     int minCostClimbingStairs(vector<int>& cost) {
          memset(t,-1,sizeof(t));
