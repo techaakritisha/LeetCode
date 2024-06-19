28f63beb22cc2098812f913f9b58ac621e7c6e05
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-20 01:38:19 +0530

    Time: 17 ms (52.65%), Space: 9.7 MB (52.25%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    /*
     int mod = 1e9+7;
     int t[31][1001];
     int solve(int n, int k , int target) {
@@ -23,5 +25,29 @@ public:
         
         memset(t,-1,sizeof(t));
         return solve(n,k,target);
+    }
+    
+    */
+    
+    
+    // BOTTOM UP
+    int mod = 1e9+7;
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n+1,vector<int>(target+1));
+        
+        dp[0][0] = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=target; j++){
+               int ways = 0;
+                for(int dice=1; dice<=k; dice++) {
+                    
+                    if(j >= dice)
+                    ways = (ways + dp[i-1][j-dice])%mod;
+                    
+                }
+                dp[i][j] = ways;
+            }
+        }
+        return dp[n][target];
     }
 };
