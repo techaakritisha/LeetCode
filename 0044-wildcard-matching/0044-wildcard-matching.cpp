a51491ff67b29c80d58cb640a1a4d35330502e05
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-26 01:52:56 +0530

    Time: 19 ms (90.54%), Space: 10.8 MB (90.74%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int n , m ;
     int dp[2001][2001];
      bool solve(int i,int j,string &s, string &p,vector<vector<int>> &dp) {
@@ -33,5 +34,41 @@ public:
         int m= p.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
         return solve(n-1,m-1,s,p,dp);
+        
+        */
+     bool isMatch(string s, string p){
+        int n = s.size();
+        int m = p.size();
+
+        bool dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        dp[0][0]=true;
+
+        // base case
+        for(int j = 1;j<=m;j++){
+            if(p[j-1]=='*'){
+                dp[0][j]=true;
+            }
+            else{
+                break;
+            }
+        }
+         
+          for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1] ;
+                }
+                else if(p[j-1] == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+                
+            }
+        }
+
+        return dp[n][m];
     }
 };
