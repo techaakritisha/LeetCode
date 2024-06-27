3feb9072f5e31d50e5cbce19b7a5056a59aa8f7f
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-27 17:32:46 +0530

    Time: 96 ms (47.50%), Space: 23.9 MB (34.26%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> directions{{-1,0},{1,0},{0,-1},{0,1}};
+    typedef pair<int,pair<int,int>> p;
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        
+        vector<vector<int>> result(m,vector<int>(n,INT_MAX));
+        
+        priority_queue<p,vector<p>,greater<p>> pq;    
+        
+        pq.push({0,{0,0}});
+        result[0][0] = 0;
+        
+        auto isSafe = [&](int x, int y){
+            return x>=0 && x<m && y>=0 && y<n ;
+        };
+        
+        
+        while(!pq.empty()){
+            int diff = pq.top().first;
+            pair<int,int> p = pq.top().second;
+            
+            int x = p.first;
+            int y = p.second;
+            
+            pq.pop();
+            
+            for(auto &dir : directions) {
+               int new_x = x + dir[0];
+               int new_y = y + dir[1];
+               
+               if(isSafe(new_x,new_y)){
+                  int absdiff = abs(heights[x][y] - heights[new_x][new_y]);
+                  int maxdiff = max(diff,absdiff);
+                   
+                  if(result[new_x][new_y] > maxdiff) {
+                      result[new_x][new_y] = maxdiff;
+                      
+                      pq.push({maxdiff,{new_x,new_y}});
+                      
+                  }
+               }
+            } 
+        }
+        
+        return result[m-1][n-1];
+    }
+};
