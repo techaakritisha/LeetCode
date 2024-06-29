80133b1ecba3dbc4f9c85247624238c4e04b0756
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-30 00:58:10 +0530

    Time: 39 ms (83.56%), Space: 10.5 MB (68.61%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    /*
+    TOP DOWN
     int t[10001];
     int solve(int n) {
         if(n == 0)
@@ -19,7 +21,19 @@ public:
         
         memset(t,-1,sizeof(t));
         return solve(n);
+        
+        */
+    
+    // BOTTOM UP
+    int numSquares(int n) {
+        vector<int> dp(n+1,100000);
+        
+        dp[0] = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j*j<=i; j++) {
+               dp[i] = min(dp[i],1+dp[i-j*j]);   
+            }
+        }
+        return dp[n];
     }
-    
-    
 };
